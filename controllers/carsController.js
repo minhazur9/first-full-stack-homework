5e835fec2cd85c94e1aac169399870f03cdcc6c2
min <minhazur9@gmail.com>
2020-10-05 19:41:24 -0400

    added show page

--- controllers/carsController.js
+++ controllers/carsController.js
@@ -20,4 +20,22 @@ router.get('/new', (req, res) => {
     res.render('cars/newCar')
 });
 
+// Create Cars
+router.post('/', (req, res) => {
+    db.Car.create(req.body, (err, createdCar) => {
+        if (err) return console.log(err);
+        res.redirect(`/cars`);
+    })
+})
+
+//Show Cars
+router.get('/:carId', (req,res) => {
+    db.Car.findById(req.params.carId, (err,foundCar) => {
+        if(err) return console.log(err);
+        res.render(`cars/showCar`,{
+            car: foundCar
+        })
+    }); 
+});
+
 module.exports = router;
