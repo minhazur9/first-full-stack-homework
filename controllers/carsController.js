b0ac90c86979ab9b98e04eb7097d1168afe1b5e0
min <minhazur9@gmail.com>
2020-10-05 18:56:37 -0400

    New car route added

--- controllers/carsController.js
+++ controllers/carsController.js
@@ -5,14 +5,19 @@ const router = express.Router();
 // Database
 const db = require('../models');
 
-
-router.get('/',(req,res) => {
-    db.Car.find({},(err,allCars) => {
-        if(err) return console.log(err);
+// Index Route
+router.get('/', (req, res) => {
+    db.Car.find({}, (err, allCars) => {
+        if (err) return console.log(err);
         res.render('cars/indexCar', {
             cars: allCars,
         })
     })
-})
+});
+
+// New Route
+router.get('/new', (req, res) => {
+    res.render('cars/newCar')
+});
 
 module.exports = router;
